a1d8991913782f2ebee233440aaeeef42ca0fd78
ElrohirGT <elrohirgt@gmail.com>
2026-02-02 14:54:04 -0600

    feat: Added basic go app

---
+++ goapp/rolldice.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+func rolldice(w http.ResponseWriter, r *http.Request) {
+	roll := 1 + rand.Intn(6)
+
+	var msg string
+	if player := r.PathValue("player"); player != "" {
+		msg = player + " is rolling the dice"
+	} else {
+		msg = "Anonymous player is rolling the dice"
+	}
+	log.Printf("%s, result: %d", msg, roll)
+
+	resp := strconv.Itoa(roll) + "\n"
+	if _, err := io.WriteString(w, resp); err != nil {
+		log.Printf("Write failed: %v", err)
+	}
+}
