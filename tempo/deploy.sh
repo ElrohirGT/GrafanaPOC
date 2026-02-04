0ab4c5cfcd9f78b0e1eca6591231d10bacfe20a9
ElrohirGT <elrohirgt@gmail.com>
2026-02-04 16:46:59 -0600

    feat: Passed from a tempo operator into a tempo single binary

--- tempo/deploy.sh
+++ tempo/deploy.sh
@@ -2,9 +2,5 @@
 
 set -exu
 
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.16.1/cert-manager.yaml
-sleep 15s
-kubectl apply -f https://github.com/grafana/tempo-operator/releases/latest/download/tempo-operator.yaml
-sleep 15s
-kubectl apply -f https://raw.githubusercontent.com/grafana/tempo-operator/main/minio.yaml
-kubectl apply -n monitoring -f .
+helm repo add grafana-community https://grafana-community.github.io/helm-charts
+helm install tempo grafana-community/tempo -n monitoring --values ./values.yml
