4cbb65855eb0d9371d5f715d96fd5f44c0c97862
ElrohirGT <elrohirgt@gmail.com>
2026-02-05 18:14:53 -0600

    fix: docker image push

--- dice-app/deploy.sh
+++ dice-app/deploy.sh
@@ -3,5 +3,5 @@
 set -exu
 
 docker build -t fagdtw/dice-app .
-docker image push fagdtw/dice-app
+docker image push fagdtw/dice-app | echo "Failed to publish the image, assuming someone published it before you..."
 kubectl apply -f ./kubernetes/
