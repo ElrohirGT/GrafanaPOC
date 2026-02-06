0978b701507612c9ded28fce14a86f4838105ce8
ElrohirGT <elrohirgt@gmail.com>
2026-02-05 18:07:43 -0600

    fix: grafana repo

--- alloy/deploy.sh
+++ alloy/deploy.sh
@@ -2,4 +2,5 @@
 
 set -exu
 
+helm repo add grafana https://grafana.github.io/helm-charts
 helm install grafana-alloy grafana/alloy -n monitoring -f alloy.yml || helm upgrade grafana-alloy grafana/alloy -n monitoring -f alloy.yml
