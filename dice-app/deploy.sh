cafcfd558d02b00bdecc409d0490878685280952
ElrohirGT <elrohirgt@gmail.com>
2026-02-12 12:34:44 -0600

    fix: deploy script

--- dice-app/deploy.sh
+++ dice-app/deploy.sh
@@ -3,5 +3,5 @@
 set -exu
 
 # docker build -t fagdtw/dice-app .
-docker buildx build --platform linux/amd64,linux/arm64 --tag fagdtw/dice-app --push . | echo "Failed to build and push image, asumming one is already published..."
+docker buildx build --platform linux/amd64,linux/arm64 --tag fagdtw/dice-app --push . || echo "Failed to build and push image, asumming one is already published..."
 kubectl apply -f ./kubernetes/
