815a8f4383088350eaa7e1d9f3394b65b8ae302d
ElrohirGT <elrohirgt@gmail.com>
2026-02-12 09:16:56 -0600

    feat: tempo distributed

--- tempo/deploy.sh
+++ tempo/deploy.sh
@@ -3,4 +3,4 @@
 set -exu
 
 helm repo add grafana-community https://grafana-community.github.io/helm-charts
-helm install tempo grafana-community/tempo -n monitoring --values ./values.yml
+helm install tempo grafana-community/tempo-distributed -n monitoring --values ./values.yml
